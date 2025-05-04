ced04b5355a0c2589960d21d9c4768ad55acf4a4
Saidjon <fozilovsaidjon71@gmail.com>
2025-05-04 11:33:10 +0300

    d

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ const PORT = process.env.PORT || 8000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+
 app.use(cors());
 app.use(express.json());
 
